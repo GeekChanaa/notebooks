230f24cccf98a126a6695ddb71e55039b4a50793
AdnaneAm <enanda.am@gmail.com>
2019-12-14 22:27:10 +0100

    last

--- public/js/notebooks.js
+++ public/js/notebooks.js
@@ -12,6 +12,7 @@ $(".notes-container>li>a").click(function(e){
 $(".subnotes-container>li>a").click(function(e){
     var num_elements=$(this).next().children().length-1;
     var total_length=$(this).next().children().last().height()*num_elements;
+    $(this).next().toggleClass('open');
     if($(this).parent().css('max-height')=="40px"){
         $(this).parent().css('max-height',total_length);
     }
