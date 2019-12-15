9f23a6d9a2df4df1f44e603d87a9c70ed63b919f
chanaa <mohammedchanaa@hotmail.com>
2019-12-15 19:23:18 +0100

    added some stuff

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -9,16 +9,36 @@ use App\chapter;
 use App\section;
 use App\notebook;
 use App\page;
+use DB;
 
 class dashboardController extends Controller
 {
+
     //Index page
     public function index(){
+      // getting current year and month
+      $year = date("Y");
+      $month = date('m', strtotime('-0 month'));
+    
+      $list_users = DB::table('notebooks')
+      ->leftJoin('sections','notebooks.id','=','sections.notebook_id')
+      ->leftJoin('chapters','chapters.section_id','=','sections.id')
+      ->leftJoin('pages','pages.chapter_id','=','chapters.id')
+      ->select('notebooks.user_id',DB::raw('count(notebooks.id) as nbr_notebooks'),DB::raw('count(sections.id) as nbr_sections'),DB::raw('count(chapters.id) as nbr_chapters'),DB::raw('count(pages.id) as nbr_pages'))
+      ->groupBy('notebooks.user_id')
+      ->get();
       $data=[
+        'nbr_users_last_month' => User::whereYear('created_at','=',$year)->whereMonth('created_at','=',$month)->count(),
+        'nbr_users_last_year' => User::whereYear('created_at','=',$year)->count(),
+        'nbr_notebooks_last_year' => notebook::whereYear('created_at','=',$year)->count(),
+        'nbr_notebooks_last_month' => notebook::whereYear('created_at','=',$year)->whereMonth('created_at','=',$month)->count(),
+        'list_users' => $list_users,
         'nbr_users' => User::count(),
         'nbr_notebooks' => notebook::count(),
         'storage_capacity' => 500,
         'nbr_connected_users' => 200,
+        'nbr_users_year' => $this->nbr_users_year(2019),
+        'nbr_notebooks_year' => $this->nbr_notebooks_year(2019)
       ];
       return view('dashboard.index')->with($data);
     }
@@ -30,6 +50,15 @@ class dashboardController extends Controller
         $nbr_users[$i] = User::whereYear('created_at','=',$year)->whereMonth('created_at','=',$i)->count();
       }
       return $nbr_users;
+    }
+
+    // Number of notebooks per year
+    protected function nbr_notebooks_year($year){
+      $nbr_notebooks = [];
+      for($i=1 ; $i<13 ; $i++){
+        $nbr_notebooks[$i] = notebook::whereYear('created_at','=',$year)->whereMonth('created_at','=',$i)->count();
+      }
+      return $nbr_notebooks;
     }
 
     // USERS STATISTICS
