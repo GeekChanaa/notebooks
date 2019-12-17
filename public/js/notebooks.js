3bbb654cd46c1b22f5fa11ae9145cceafb9d400f
AdnaneAm <enanda.am@gmail.com>
2019-12-17 07:09:47 +0100

    9riiiib

--- public/js/notebooks.js
+++ public/js/notebooks.js
@@ -1,23 +1,39 @@
+// Array of colors
+var colors = ['#3498db', '#e67e22', '#27ae60', '#9b59b6','#1abc9c','#34495e'];
+
+
 // First sidebar animation
 $(".notes-container>li>a").click(function(e){
   var num_elements=$(this).next().children().length;
-  var total_length=$(this).next().children().height()*num_elements;
+  var total_length=($(this).next().find("a").outerHeight())*num_elements;
   if($(this).next().css('max-height')=="0px"){
       $(this).css({
-        'color':'#fff',
-        'padding':'1.5em 0 1.5em 3.5em'
+        'padding':'1.5em 0 1.5em 3.7em',
+        'color':$(this).siblings(".note-bg").css('background-color'),
+        'font-size':'13px'
       });
       $(this).next().css('max-height',total_length);
-      $(this).siblings(".note-bg").css('width','100%');
+      $(this).siblings(".note-bg").css('height',total_length+59);
+      $(this).siblings("svg").css({
+        'fill':$(this).siblings(".note-bg").css('background-color'),
+        'width':'14px',
+        'height':'14px'
+      });
   }
   else{
       $(this).css({
-        'color':'#000',
-        'padding':'1.5em 0 1.5em 5em'
+        'padding':'1.5em 0 1.5em 4.2em',
+        'color':'#8a8a8a',
+        'font-size':'13px'
       });
       $(this).next().css('max-height',0);
       $(this).next().find('.lastnotes-container.open').removeClass('open');
-      $(this).siblings(".note-bg").css('width','59px');
+      $(this).siblings(".note-bg").css('height','59px');
+      $(this).siblings("svg").css({
+        'fill':'#525252',
+        'width':'15px',
+        'height':'15px'
+      });
 
   }
 });
@@ -33,9 +49,16 @@ $(".subnotes-container>li>a").click(function(e){
 
 // Coloring the different notes
 
-
-var colors = ['#e74c3c', '#3498db', '#27ae60', '#9b59b6','#f1c40f','#1abc9c','#2c3e50','#d35400'];
-
 $(".notes-container>li .note-bg").each(function(index){
   $(this).css('background',colors[index]);
+});
+
+// Inserting the icons from jquery for a clean html code
+
+$(".subnotes-container>li>a").prepend($(".subnotes-container>li>a").prev("#arrow-right"));
+
+// Coloring the arrows of each section
+
+$(".notes-container>li").each(function(index){
+  $(this).find(".subnotes-container svg").css('fill',colors[index]);
 });
--- resources/views/desktop/notebooks.blade.php
+++ resources/views/desktop/notebooks.blade.php
@@ -13,16 +13,20 @@
           <li>
             <div class="note-bg">
             </div>
-            <svg version="1.1" id="bookmark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            	 viewBox="0 0 56.868 56.868" style="enable-background:new 0 0 56.868 56.868;" xml:space="preserve">
-            	<path d="M46.934,0.011V0h-36.91c-3.358,0-6.09,2.731-6.09,6.09v50.778l19-12.666l19,12.666V32v-7V14h2v11h9V6.136
-            		C52.934,2.819,50.262,0.096,46.934,0.011z"/>
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            	 width="24.75px" height="24.75px" viewBox="0 0 24.75 24.75" style="enable-background:new 0 0 24.75 24.75;" xml:space="preserve"
+            	>
+            	<g>
+            		<path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2
+            			c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z M22.75,18.875H2
+            			c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z"/>
+            	</g>
             </svg>
             <a href="#">Projects</a>
             <ul class="subnotes-container">
               <li>
                 <a href="#">
-                  Something 1
+                  Badir Web
                 </a>
                 <ul class="lastnotes-container">
                   <li><a href="#">Another1</a></li>
@@ -33,7 +37,7 @@
               </li>
               <li>
                 <a href="#">
-                  Something 2
+                  Fissoune BD
                 </a>
                 <ul class="lastnotes-container">
                   <li><a href="#">New1</a></li>
@@ -47,10 +51,57 @@
           <li>
             <div class="note-bg">
             </div>
-            <svg version="1.1" id="bookmark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            viewBox="0 0 56.868 56.868" style="enable-background:new 0 0 56.868 56.868;" xml:space="preserve">
-            <path d="M46.934,0.011V0h-36.91c-3.358,0-6.09,2.731-6.09,6.09v50.778l19-12.666l19,12.666V32v-7V14h2v11h9V6.136
-            C52.934,2.819,50.262,0.096,46.934,0.011z"/>
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            	 viewBox="0 0 298 298" style="enable-background:new 0 0 298 298;" xml:space="preserve">
+            	<g>
+            	<path d="M117.5,0h-101c-8.822,0-16,7.178-16,16v101c0,8.822,7.178,16,16,16h101c8.822,0,16-7.178,16-16V16
+            		C133.5,7.178,126.322,0,117.5,0z"/>
+            	<path d="M281.5,0h-101c-8.822,0-16,7.178-16,16v101c0,8.822,7.178,16,16,16h101c8.822,0,16-7.178,16-16V16
+            		C297.5,7.178,290.322,0,281.5,0z"/>
+            	<path d="M117.5,165h-101c-8.822,0-16,7.178-16,16v101c0,8.822,7.178,16,16,16h101c8.822,0,16-7.178,16-16V181
+            		C133.5,172.178,126.322,165,117.5,165z"/>
+            	<path d="M281.5,165h-101c-8.822,0-16,7.178-16,16v101c0,8.822,7.178,16,16,16h101c8.822,0,16-7.178,16-16V181
+            		C297.5,172.178,290.322,165,281.5,165z"/>
+            	</g>
+            </svg>
+            <a href="#">Todos</a>
+            <ul class="subnotes-container">
+              <li>
+                <svg version="1.1" id="arrow-right" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                	 viewBox="0 0 448.011 448.011" style="enable-background:new 0 0 448.011 448.011;" xml:space="preserve">
+                	<g>
+                		<path d="M438.731,209.463l-416-192c-6.624-3.008-14.528-1.216-19.136,4.48c-4.64,5.696-4.8,13.792-0.384,19.648l136.8,182.4
+                			l-136.8,182.4c-4.416,5.856-4.256,13.984,0.352,19.648c3.104,3.872,7.744,5.952,12.448,5.952c2.272,0,4.544-0.48,6.688-1.472
+                			l416-192c5.696-2.624,9.312-8.288,9.312-14.528S444.395,212.087,438.731,209.463z"/>
+                	</g>
+                </svg>
+                <a href="#">
+                  Java
+                </a>
+                <ul class="lastnotes-container">
+                  <li><a href="#">Hopaa 1</a></li>
+                  <li><a href="#"> Hopaa 2</a></li>
+                  <li><a href="#"> Hopaa 3</a></li>
+                  <li><a href="#"> Hopaa 4</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">
+                  Machine Learning
+                </a>
+                <ul class="lastnotes-container">
+                  <li><a href="#">Test 1</a></li>
+                  <li><a href="#"> Test 2</a></li>
+                  <li><a href="#"> Test 3</a></li>
+                  <li><a href="#"> Test 4</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div class="note-bg">
+            </div>
+            <svg id="Capa_1" enable-background="new 0 0 515.555 515.555" height="512" viewBox="0 0 515.555 515.555" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m496.679 212.208c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138 65.971-25.167 91.138 0"/><path d="m303.347 212.208c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138 65.971-25.167 91.138 0"/><path d="m110.014 212.208c25.167 25.167 25.167 65.971 0 91.138s-65.971 25.167-91.138 0-25.167-65.971 0-91.138 65.971-25.167 91.138 0"/>
             </svg>
             <a href="#">Todos</a>
             <ul class="subnotes-container">
